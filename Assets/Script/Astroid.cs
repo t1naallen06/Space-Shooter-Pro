66a2c4d82ff49d9fcd94ca3f595379d8669079d8
Tina Allen <t1naAllen06@outlook.com>
2021-09-22 13:24:02 -0500

    explode astroid

--- Assets/Script/Astroid.cs
+++ Assets/Script/Astroid.cs
@@ -7,7 +7,8 @@ public class Astroid : MonoBehaviour
 
     [SerializeField]
     private float _rotateSpeed = 19.0f;
-   
+    [SerializeField]
+    private GameObject _explosionPrefab;
   
     // Update is called once per frame
     void Update()
@@ -20,4 +21,15 @@ public class Astroid : MonoBehaviour
     //check for laser collision (trigger)
     //instantiate exsplosion at the position of asteroid (us)
     //destroy the exslosion agter 3 seconds
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject, 0.25f);
+
+        }
+
+    }
 }
